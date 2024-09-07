fe4d58ff0abeaec0411b545eba8eb8740a0b726d
NickBrisebois <nickbrisebois12@gmail.com>
2024-09-07 15:16:17 -0400

    feat: update model

--- chat_ai/chatai.py
+++ chat_ai/chatai.py
@@ -22,7 +22,7 @@ class ChatAI:
     async def get_response(self, user_id: int, input_text: str) -> str:
         self._conversation_history.append({"role": "user", "content": input_text})
         response = await self._client.chat.completions.create(
-            model="ft:gpt-3.5-turbo-1106:personal:lezbetterinput1k:A4bfc0Kg",
+            model="ft:gpt-3.5-turbo-0125:personal:lez60k-better:A4vAxeMd",
             messages=self._conversation_history,
             max_tokens=500,
         )
