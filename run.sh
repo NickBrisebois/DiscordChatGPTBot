29771ba7b2e69baa6c5001e49b7a2d369aa6e218
NickBrisebois <nickbrisebois12@gmail.com>
2026-01-07 09:43:01 -0500

    chore: update dockerfile for uv, add codeowners

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 # Mostly used by the docker container.
 # Remove the /chatai/ if you want to use this to run the bot locally.
-
-/usr/bin/env poetry run python3 /chatai/main.py
+/usr/bin/env uv run python /chatai/main.py
